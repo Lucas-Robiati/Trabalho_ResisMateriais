3594062c40b8fcb6e15ea552cee55ac96f9d3cab
Lucas-Robiati <lucas.santana.280304@gmail.com>
2025-04-20 19:10:23 -0300

    frames

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 #fazer botão tela cheia
 #self.root.attributes('-fullscreen', True)
 
+#cinza '#5e5c64'
+
 from tkinter import *
+from enum import Enum
 
 from Formas import Retangulo
 from Formas import Ponto2D
+
+class Color(Enum):
+    gray = "#5e5c64"
+    dark_blue = "#18304a"
 
 root = Tk()
 
@@ -12,13 +19,19 @@ class Application():
     def __init__(self):
         self.root = root
         self.window()
+        self.window_frame()
         root.mainloop()
 
     def window(self):
         self.root.title("Calculadora de Momento de Inercia")
-        self.root.configure(background='#5e5c64')
+        self.root.configure(background= Color.dark_blue.value)
         self.root.geometry("950x520")
-        self.root.minsize(700, 500)
+        self.root.minsize(width=700, height=500)
         self.root.resizable(True,True)
-        
+
+    def window_frame(self):
+        self.main_frame = Frame(self.root)
+        self.main_frame.place(relx=0.1, rely=0.1, relwidth=0.5, relheight=0.9)
+        self.main_frame.configure(background= Color.gray.value)
+
 Application()
