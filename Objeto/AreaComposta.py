21598a96d5da1b926cea49e723fba7c7681aaa78
Edson <edsonaugustotl@gmail.com>
2025-04-21 01:50:29 -0400

    iniciando calculo de centroide de areas compostas

--- Objeto/AreaComposta.py
+++ Objeto/AreaComposta.py
@@ -1,5 +1,20 @@
 from Formas import Forma
+from Formas import Ponto2D
 
 class AreaComposta(Forma):
   def __init__(self):
+    self.componentes = []
+    self.centroide = self._c_centroide()
+    self.Ix, self.Iy = self.momento()
     return
+  
+
+  def _c_centroide(self) -> Ponto2D:
+    if(not self.componentes):
+      
+      return Ponto2D()
+    
+    return Ponto2D()
+  
+  def momento(self):
+    return 0.0, 0.0
