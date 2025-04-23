7fbfe0960b912807ed7f772d3caaf54a113cbd91
Lucas-Robiati <lucas.santana.280304@gmail.com>
2025-04-22 21:25:00 -0300

    ta dando errado

---
+++ modulos.py
@@ -0,0 +1,14 @@
+
+from tkinter import *
+from enum import Enum
+
+import Formas
+import Formas
+
+class Color(Enum):
+    gray = "#5e5c64"
+    light_gray = "#868687"
+    dark_blue = "#18304a"
+    aqua_blue = "#033f70"
+    white = "#e8e8ed"
+    black = "#000000"
